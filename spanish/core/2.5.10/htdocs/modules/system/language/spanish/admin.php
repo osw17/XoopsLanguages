d49fdfe9ed0456f73d8e5d817d029135ab6170ca
osw17 <osw17@hotmail.com>
2020-03-17 17:15:05 -0600

    Update admin.php
    Update translation for this file

--- spanish/core/2.5.10/htdocs/modules/system/language/spanish/admin.php
+++ spanish/core/2.5.10/htdocs/modules/system/language/spanish/admin.php
@@ -1,54 +1,76 @@
 <?php
-//
-define('_OXYGEN_SYSOPTIONS', 'Opciones del Sistema');
-define('_OXYGEN_INSTALLEDMODULES', 'Módulos Instalados');
-define('_OXYGEN_XOOPSTHEMES', 'Diseños');
-define('_OXYGEN_XOOPSMODULES', 'Módulos');
-define('_OXYGEN_INTERESTSITES', 'Enlaces');
-define('_OXYGEN_LOCALSUPPORT', 'Soporte Local');
-define('_OXYGEN_NEWS', 'Noticias');
-define('_OXYGEN_WEBSITE', 'Comunidad');
-define('_OXYGEN_SOURCEFORGE', 'Sourceforge');
-define('_OXYGEN_VERSION', 'Versión');
-define('_OXYGEN_VERSION_XOOPS', 'Versión del Sistema');
-define('_OXYGEN_VERSION_PHP', 'Versión PHP');
-define('_OXYGEN_VERSION_MYSQL', 'Versión mySQL');
-define('_OXYGEN_Server_API', 'API del Servidor');
-define('_OXYGEN_OS', 'SO');
-define('_OXYGEN_ABOUT', 'Acerca de XOOPS');
-define('_OXYGEN_ABOUT_TEXT', 'Lea la página <a href="http://www.xoops.org/modules/wfchannel/" rel="external">Todo sobre XOOPS</a> para más detalles.');
-define('_OXYGEN_XOOPS_LINKS', 'Enlaces');
-define('_OXYGEN_XOOPSPROJECT', 'Proyecto XOOPS');
-define('_OXYGEN_XOOPSCORE', 'XOOPS Core');
-define('_OXYGEN_XOOPSTHEME', 'Diseños');
-define('_OXYGEN_XOOPSWIKI', 'XOOPS Wiki');
-define('_OXYGEN_XOOPSBOOKS', 'Libros');
-define('_OXYGEN_NEWMODULE', 'Módulos Nuevos');
-define('_OXYGEN_XOOPSFAQ', 'XOOPS FAQ');
-define('_OXYGEN_CODESVN', 'Repositorio de Código');
-define('_OXYGEN_REPORTBUG', 'Reportar un Error');
-define('_OXYGEN_SITEPREF', 'Preferencias');
-define('_OXYGEN_GENERAL', 'Configuración General');
-define('_OXYGEN_USERSETTINGS', 'Configuración de Información del Usuario');
-define('_OXYGEN_METAFOOTER', 'Meta Etiquetas y Pie de Página');
-define('_OXYGEN_CENSOR', 'Opciones de Censura de Palabras');
-define('_OXYGEN_SEARCH', 'Opciones de Búsqueda');
-define('_OXYGEN_MAILER', 'Configuración del Correo');
-define('_OXYGEN_AUTHENTICATION', 'Opciones de Autenticación');
-define('_OXYGEN_MODULESETTINGS', 'Configuración del Módulo del Sistema');
-//Add for styles name
-define('_OXYGEN_SILVER', 'Plata');
-define('_OXYGEN_DARK', 'Oscuro');
-define('_OXYGEN_ORANGE', 'Naranja');
-//Add 10 des
-define('_OXYGEN_XOOPS_LICENSE', 'Licencia');
-define('_OXYGEN_RSS', 'Alimentación RSS');
-define('_OXYGEN_ADMINISTRATION', 'Administración');
-define('_OXYGEN_UPTOP', 'Arriba');
-//Add help
-define('_OXYGEN_HELP_1', '¿Cómo puedo crear contenido con XOOPS?');
-define('_OXYGEN_HELP_DESC_1', 'Para crear contenido, primero debe instalar un módulo de contenido, como el módulo <a href="https://github.com/XoopsModules25x/publisher" rel="external">Publisher</a> o <a href="https://github.com/XoopsModules25x/news" rel="external">News</a>. Para obtener más información sobre los módulos de XOOPS, <a href="modules/system/help.php?mid=1&page=modulesadmin">haga clic aquí</a>');
-define('_OXYGEN_HELP_2', '¿Qué es el Bloque?');
-define('_OXYGEN_HELP_DESC_2', 'Los bloques pueden proporcionar contenido adicional / específico de los módulos instalados. Puede haber bloques personalizados que pueden contener texto, código JS, texto con formato HTML, imágenes, etc. El contenido de estos bloques puede formatearse individualmente o puede heredar el formato del sitio web. (<a href="modules/system/help.php?mid=1&page=blocksadmin">más ... </a>)');
-define('_OXYGEN_HELP_3', '¿Cómo puedo encontrar más ayuda?');
-define('_OXYGEN_HELP_DESC_3', 'Si necesita más ayuda e información para usar XOOPS, puede usar <a href="modules/system/help.php">Las Páginas de Ayuda </a> en el módulo del sistema o usar <a href="http://www.xoops.org/modules/xoopspartners/" rel="external">Sitios de soporte local de XOOPS </a>');
+/**
+ * @copyright       (c) 2000-2016 XOOPS Project (www.xoops.org)
+ * @license             GNU GPL 2 (http://www.gnu.org/licenses/gpl-2.0.html)
+ * _LANGCODE    es
+ * _CHARSET     UTF-8
+ */
+// Main
+define('_AM_SYSTEM_CONFIG', 'Configuración del Sistema');
+define('_AM_SYSTEM_CPANEL', 'Panel de Control');
+define('_AM_SYSTEM_UPDATE', 'Actualizar');
+define('_AM_SYSTEM_GOTOMODULE', 'Ir al Módulo');
+define('_AM_SYSTEM_HELP', 'Help');
+define('_AM_SYSTEM_HELP_VIEW', 'Mostrar Ayuda');
+define('_AM_SYSTEM_HELP_HIDE', 'Ocultar Ayuda');
+define('_AM_SYSTEM_TIPS', 'Consejos');
+define('_AM_SYSTEM_SECTION', 'Sección');
+define('_AM_SYSTEM_DESC', 'Descripción');
+define('_AM_SYSTEM_GO', 'Acceda a esta sección');
+define('_AM_SYSTEM_STATUS', 'Cambiar el Estado de la Sección');
+define('_AM_SYSTEM_LOADING', 'Cargando');
+define('_AM_SYSTEM_ALL', 'All');
+define('_AM_SYSTEM_TIPS_MAIN', '<ul><li>Activa o desactiva secciones del módulo del sistema o solo acceso a ella.</li></ul>');
+define('_AM_SYSTEM_AVATAR_INFO', "<ul><li><span class='bold red'>%s</span> avatares.</li></ul>");
+define('_AM_SYSTEM_BANNER_INFO', "<ul><li><span class='bold red'>%s</span> banners.</li></ul>");
+define('_AM_SYSTEM_COMMENT_INFO', "<ul><li><span class='bold red'>%s</span> commentarios.</li></ul>");
+define('_AM_SYSTEM_GROUP_INFO', "<ul><li><span class='bold red'>%s</span> grupos.</li></ul>");
+define('_AM_SYSTEM_IMG_INFO', "<ul><li><span class='bold red'>%s</span> imágenes.</li></ul>");
+define('_AM_SYSTEM_SMILIES_INFO', "<ul><li><span class='bold red'>%s</span> emoticonos.</li></ul>");
+define('_AM_SYSTEM_RANKS_INFO', "<ul><li><span class='bold red'>%s</span> rangos de usuarios.</li></ul>");
+define('_AM_SYSTEM_USERS_INFO', "<ul><li><span class='bold red'>%s</span> usuarios.</li></ul>");
+// Admin Module Names and description
+define('_AM_SYSTEM_ADGS', 'Grupos');
+define('_AM_SYSTEM_ADGS_DESC', 'Gestiona permisos de acceso a módulos para usuarios y grupos.');
+define('_AM_SYSTEM_BANS', 'Banners');
+define('_AM_SYSTEM_BANS_DESC', 'Administra la función de banner publicitario.');
+define('_AM_SYSTEM_BLOCKS', 'Bloques');
+define('_AM_SYSTEM_BLOCKS_DESC', 'Los bloques pueden mostrar el contenido del módulo en cualquier página. Gestionar bloques aquí.');
+define('_AM_SYSTEM_MODULES', 'Módulos');
+define('_AM_SYSTEM_MODULES_DESC', 'Instalar y desinstalar módulos');
+define('_AM_SYSTEM_SMLS', 'Emoticonos');
+define('_AM_SYSTEM_SMLS_DESC', 'Ocultar, cambiar y agregar emoticones personalizados para que las personas los usen en publicaciones y comentarios.');
+define('_AM_SYSTEM_RANK', 'Rangos de Usuario');
+define('_AM_SYSTEM_RANK_DESC', 'Edite y cree rangos con nombre para usuarios y moderadores.');
+define('_AM_SYSTEM_USER', 'Usuarios');
+define('_AM_SYSTEM_USER_DESC', 'Agregue usuarios manualmente, edite perfiles de usuario y cambie las contraseñas.');
+define('_AM_SYSTEM_PREF', 'Preferencias');
+define('_AM_SYSTEM_PREF_DESC', 'Cambie las preferencias globales para su sitio web ');
+define('_AM_SYSTEM_MLUS', 'Correo a Usuarios');
+define('_AM_SYSTEM_MLUS_DESC', 'Envíe correos electrónicos o mensajes privados a un usuario, o a muchos usuarios desde aquí.');
+define('_AM_SYSTEM_IMAGES', 'Administrador de imágenes');
+define('_AM_SYSTEM_IMAGES_DESC', 'Cree categorías para el administrador de imágenes y suba las imágenes aquí.');
+define('_AM_SYSTEM_AVATARS', 'Avatares');
+define('_AM_SYSTEM_AVATARS_DESC', 'Cargue avatares personalizados para que los usuarios los muestren en sus perfiles.');
+define('_AM_SYSTEM_TPLSETS', 'Plantillas');
+define('_AM_SYSTEM_TPLSETS_DESC', 'Edite plantillas de módulos directamente, sin editar los archivos en el disco.');
+define('_AM_SYSTEM_COMMENTS', 'Commentarios');
+define('_AM_SYSTEM_COMMENTS_DESC', 'Muchos módulos permiten a los usuarios publicar comentarios. Puede eliminarlos o editarlos aquí.');
+define('_AM_SYSTEM_FILEMANAGER', 'Administrador de Archivos');
+define('_AM_SYSTEM_FILEMANAGER_DESC', 'Administrador de Archivos del Sistema.');
+define('_AM_SYSTEM_MAINTENANCE', 'Mantenimiento');
+define('_AM_SYSTEM_MAINTENANCE_DESC', 'Herramientas de mantenimiento para tablas de base de datos, carpetas de caché y tabla de sesión.');
+// Messages
+define('_AM_SYSTEM_DBUPDATED', 'Base de datos actualizada con éxito!');
+define('_AM_SYSTEM_DBERROR', '¡La base de datos no se actualizó debido a algún error!');
+define('_AM_SYSTEM_NOTACTIVE', '¡Esta sección no está activa!');
+// Group permission phrases
+define('_MD_AM_PERMADDNG', 'No se pudo agregar el permiso %s a %s para el grupo %s');
+define('_MD_AM_PERMADDOK', 'Se agregó el permiso %s a %s para el grupo %s');
+define('_MD_AM_PERMRESETNG', 'No se pudo restablecer el permiso de grupo para el módulo %s');
+define('_MD_AM_PERMADDNGP', 'Todos los elementos principales deben ser seleccionados.');
+define('_AM_SYSTEM_UNINSTALL', 'Desinstalar');
+
+//2.5.7
+define('_AM_SYSTEM_USAGE', 'Uso');
+define('_AM_SYSTEM_ACTIVE', 'Activo');
